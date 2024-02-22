5e5be57bc5282c5820432217adbe50b6215c8ece
Leila Peltier <l_peltier@outlook.com>
2024-02-22 22:57:14 +0000

    Refactored my  controller to use the greeting  POJO instead of a string

--- task_01/src/main/java/com/example/task_01/controllers/Controller.java
+++ task_01/src/main/java/com/example/task_01/controllers/Controller.java
@@ -1,13 +1,25 @@
 package com.example.task_01.controllers;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import com.example.task_01.models.GreetingPojo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/greeting")
 public class Controller{
-    @GetMapping("/greeting")
-    public String greeting(){
-        return "Good afternoon!";
+
+ // Old method:
+//    @GetMapping("/greeting")
+//    public String greeting(){
+//        return "Good afternoon!";
+//    }
+
+    @GetMapping
+    public GreetingPojo greetingPojo(){
+        GreetingPojo greeting = new GreetingPojo("Leila","afternoon");
+        return greeting;
     }
+
 
 
 
